017ba23ef90202856bb209ffc3899ba2c534cdfa
bucilamihai <77799794+bucilamihai@users.noreply.github.com>
2025-06-25 21:27:41 +0300

    added requirements to deploy recommendation system

--- app/recommendation-system/app.py
+++ app/recommendation-system/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -40,4 +41,4 @@ def recommend():
     return jsonify(recommendations), 200
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
